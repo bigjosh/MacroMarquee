9962bbbdd95caefc0cfcd1ff88cabb9b74f6b484
Josh Levine <github@junk.josh.com>
2016-05-18 14:30:36 -0400

    Final (?) Cleanup

--- Arduino/MacroMarquee/MacroMarquee.ino
+++ Arduino/MacroMarquee/MacroMarquee.ino
@@ -14,12 +14,11 @@
 #define PIXEL_PORT  PORTD  // Port of the pin the pixels are connected to
 #define PIXEL_DDR   DDRD   // Port of the pin the pixels are connected to
 
-#define PIXEL_BITMASK 0b11111110  // If you do not want to use all 8 pins, you can mask off the ones you don't want
-                                  // Note that these will still get 0 written to them when we send pixels
-                                  // TODO: If we have time, we could even add a variable that will and/or into the bits before writing to the port to support any combination of bits/values
 
-static const uint8_t onBits=0xfe;        // Bit pattern to write to port to turn on all pins connected to LED strips. 
-                                  
+static const uint8_t onBits=0b11111110;   // Bit pattern to write to port to turn on all pins connected to LED strips. 
+                                          // If you do not want to use all 8 pins, you can mask off the ones you don't want
+                                          // Note that these will still get 0 written to them when we send pixels
+                                          // TODO: If we have time, we could even add a variable that will and/or into the bits before writing to the port to support any combination of bits/values                                  
 
 // These are the timing constraints taken mostly from the WS2812 datasheets 
 // These are chosen to be conservative and avoid problems rather than for maximum throughput 
@@ -42,21 +41,16 @@ static const uint8_t onBits=0xfe;        // Bit pattern to write to port to turn
 
 #define NS_TO_CYCLES(n) ( (n) / NS_PER_CYCLE )
 
-// Actually send the next set of 8 bits to the 8 pins.
-// We must to drop to asm to enusre that the complier does
-// not reorder things and make it so the delay happens in the wrong place.
 
-// OnBits is the mask of which bits are being used (PIXEL_BITMASK). We pass it on so that we
-// do not turn on unused pins becuae this would enable the pullup. Also, hopefully passing this
-// will cause the compiler to allocate a Register for it and avoid a reload every pass.
-
-// TODO: We could actually compute the next color byte translation while the inital bit phase is bring transmitted to do some pipelining....
-
-// Send a full 8 bits down all the pins, represening a single color of 1 pixel
+// Sends a full 8 bits down all the pins, represening a single color of 1 pixel
 // We walk though the 8 bits in colorbyte one at a time. If the bit is 1 then we send the 8 bits of row out. Otherwise we send 0. 
 // We send onBits at the first phase of the signal generation. We could just send 0xff, but that mught enable pull-ups on pins that we are not using. 
 
 /// Unforntunately we have to drop to ASM for this so we can interleave the computaions durring the delays, otherwise things get too slow.
+
+// OnBits is the mask of which bits are connected to strips. We pass it on so that we
+// do not turn on unused pins becuase this would enable the pullup. Also, hopefully passing this
+// will cause the compiler to allocate a Register for it and avoid a reload every pass.
 
 static inline void sendBitx8(  const uint8_t row , const uint8_t colorbyte , const uint8_t onBits ) {  
               
@@ -103,10 +97,7 @@ static inline void sendBitx8(  const uint8_t row , const uint8_t colorbyte , con
 
     );
                                   
-
-    // Note that the inter-bit gap can be as long as you want as long as it doesn't exceed the 5us reset timeout (which is A long time)
-    // Here I have been generous and not tried to squeeze the gap tight but instead erred on the side of lots of extra time.
-    // This has thenice side effect of avoid glitches on very long strings becuase 
+    // Note that the inter-bit gap can be as long as you want as long as it doesn't exceed the reset timeout (which is A long time)
     
 } 
 
@@ -118,6 +109,9 @@ void show() {
 }
 
 
+// Send 3 bytes of color data (R,G,B) for a signle pixel down all the connected stringsat the same time
+// A 1 bit in "row" means send the color, a 0 bit means send black. 
+
 static inline void sendRowRGB( uint8_t row ,  uint8_t r,  uint8_t g,  uint8_t b ) {
 
   sendBitx8( row , g , onBits);    // WS2812 takes colors in GRB order
@@ -125,6 +119,8 @@ static inline void sendRowRGB( uint8_t row ,  uint8_t r,  uint8_t g,  uint8_t b 
   sendBitx8( row , b , onBits);    // WS2812 takes colors in GRB order
   
 }
+
+// Turn off all pixels
 
 static inline void clear() {
 
@@ -139,7 +135,6 @@ static inline void clear() {
 
 // This nice 5x7 font from here...
 // http://sunge.awardspace.com/glcd-sd/node4.html
-
 
 // Font details:
 // 1) Each char is fixed 5x7 pixels. 
@@ -255,11 +250,12 @@ const uint8_t Font5x7[] PROGMEM = {
 0x10,0x38,0x54,0x10,0x10,// 
 };
 
-// Send the pixels to form the specified char, not including intercase space
+// Send the pixels to form the specified char, not including interchar space
 // skip is the number of pixels to skip at the begining to enable sub-char smooth scrolling
 
 // TODO: Subtract the offset from the char before starting the send sequence to save time if nessisary
 // TODO: Also could pad the begining of the font table to aovid the offset subtraction at the cost of 20*8 bytes of progmem
+// TODO: Could pad all chars out to 8 bytes wide to turn the the multiply by FONT_WIDTH into a shift 
 
 static inline void sendChar( uint8_t c ,  uint8_t skip , uint8_t r,  uint8_t g,  uint8_t b ) {
 
@@ -298,6 +294,9 @@ static inline void sendString( const char *s , uint8_t skip ,  const uint8_t r, 
 
   }
 }
+
+// A nice arcade font from...
+// http://jared.geek.nz/2014/jan/custom-fonts-for-microcontrollers
 
 #define ALTFONT_WIDTH 8
 
@@ -399,10 +398,11 @@ const uint8_t altfont[] PROGMEM = {
   0x00,0xfe,0x02,0xfe,0x00,0x00,0x00,0x00,// ~
 };
 
-// Special all-your-base font 6x7
 
+// Keep track of where we are in the color cycle between chars
 static uint8_t altbright =0; 
 
+// Send a char with a column-based color cycle
 static inline void sendCharAlt( uint8_t c ) {
 
   const uint8_t *charbase = altfont + (( c -' ')* ALTFONT_WIDTH) ; 
@@ -421,8 +421,7 @@ static inline void sendCharAlt( uint8_t c ) {
 
 }
 
-// Show the passed string. The last letter of the string will be in the rightmost pixels of the display.
-// Skip is how many cols of the 1st char to skip for smooth scrolling
+// Show the passed string with the arcade font and a nice vertical color cycle effect
 
 static inline void sendStringAlt( const char *s  ) {
 
@@ -442,48 +441,20 @@ static inline void sendStringAlt( const char *s  ) {
 }
 
 
-/*
-
-  The following three functions are the public API:
-  
-  ledSetup() - set up the pin that is connected to the string. Call once at the begining of the program.  
-  sendPixel( r g , b ) - send a single pixel to the string. Call this once for each pixel in a frame.
-  show() - show the recently sent pixel on the LEDs . Call once per frame. 
-  
-*/
-
 
 // Set the specified pins up as digital out
 
 void ledsetup() {
 
-  PIXEL_DDR = 0xff; // TODO: FIX PIXEL_BITMASK;    // Set all used pins to output
-  
-  
+  PIXEL_DDR |= onBits;   // Set all used pins to output
+    
 }
 
-/*
-
-  That is the whole API. What follows are some demo functions rewriten from the AdaFruit strandtest code...
-  
-  https://github.com/adafruit/Adafruit_NeoPixel/blob/master/examples/strandtest/strandtest.ino
-  
-  Note that we always turn off interrupts while we are sending pixels becuase an interupt
-  could happen just when we were in the middle of somehting time sensitive.
-  
-  If we wanted to minimize the time interrupts were off, we could instead 
-  could get away with only turning off interrupts just for the very brief moment 
-  when we are actually sending a 0 bit (~1us), as long as we were sure that the total time 
-  taken by any interrupts + the time in our pixel generation code never exceeded the reset time (5us).
-  
-*/
 
 
 void setup() {
     
   ledsetup();  
-  DDRB=0x01;
-  delay(100);
   
 }
 
@@ -516,7 +487,7 @@ void showcountdown() {
 
   // Start sequence.....
 
-  const char *countdownstr = "      TOTAL WORLD DOMINATION BEGINS IN ";
+  const char *countdownstr = "            FIRE ROLLERBALL IN ";
 
   unsigned int count = 600; 
 
@@ -553,6 +524,8 @@ void showcountdown() {
 
   count = 100;
 
+  // One last farewell blink
+
   while (count>0) {
 
     count--;
@@ -584,10 +557,9 @@ void showstarfield() {
  
   uint8_t sectors = (PIXELS / field);      // Repeating sectors makes for more stars and faster update
 
-  for(unsigned int i=0; i<500;i++) {
+  for(unsigned int i=0; i<300;i++) {
 
     unsigned int r = random( PIXELS * 8 );   // Random slow, so grab one big number and we will break it down. 
-    
 
     unsigned int x = r /8; 
     uint8_t y = r & 0x07;                // We use 7 rows
@@ -750,9 +722,9 @@ void showinvaders() {
 
 void showallyourbase() {
   
-  const char *allyourbase = "CAT: ALL YOUR BASE ARE BELONG TO US !!! " ;
+  const char *allyourbase = "CAT: ALL YOUR BASE ARE BELONG TO US !!!" ;
 
-   clear();
+  clear();
   for(unsigned int slide=10000; slide ; slide-=10 ) {
       altbright = (slide & 0xff);
       cli();      
@@ -765,44 +737,6 @@ void showallyourbase() {
 }
 
   
-  const char *m = 
-          
-"Twas brillig, and the slithy toves "
-      "Did gyre and gimble in the wabe: "
-"All mimsy were the borogoves, "
-      "And the mome raths outgrabe. "
-
-"Beware the Jabberwock, my son! "
-      "The jaws that bite, the claws that catch! "
-"Beware the Jubjub bird, and shun "      
-      "The frumious Bandersnatch! "
-
-"He took his vorpal sword in hand; "
-      "Long time the manxome foe he sought- "
-"So rested he by the Tumtum tree "
-      "And stood awhile in thought. "
-
-"And, as in uffish thought he stood, "
-      "The Jabberwock, with eyes of flame, "
-"Came whiffling through the tulgey wood, "      
-      "And burbled as it came! "
-
-"One, two! One, two! And through and through "
-      "The vorpal blade went snicker-snack! "
-"He left it dead, and with its head "
-      "He went galumphing back. "
-
-"And hast thou slain the Jabberwock? "
-      "Come to my arms, my beamish boy! "
-"O frabjous day! Callooh! Callay! "
-      "He chortled in his joy. "
-
-"Twas brillig, and the slithy toves "
-      "Did gyre and gimble in the wabe: "
-"All mimsy were the borogoves, "
-      "And the mome raths outgrabe."  
-
-      ;
 
 #define JAB_MAX_BRIGHTNESS 0xff
 #define JAB_MIN_BRIGHTNESS 0x00
@@ -810,6 +744,46 @@ void showallyourbase() {
 
 void showjabber() {
 
+  const char *m = 
+          
+      "Twas brillig, and the slithy toves "
+            "Did gyre and gimble in the wabe: "
+      "All mimsy were the borogoves, "
+            "And the mome raths outgrabe. "
+      
+      "Beware the Jabberwock, my son! "
+            "The jaws that bite, the claws that catch! "
+      "Beware the Jubjub bird, and shun "      
+            "The frumious Bandersnatch! "
+      
+      "He took his vorpal sword in hand; "
+            "Long time the manxome foe he sought- "
+      "So rested he by the Tumtum tree "
+            "And stood awhile in thought. "
+      
+      "And, as in uffish thought he stood, "
+            "The Jabberwock, with eyes of flame, "
+      "Came whiffling through the tulgey wood, "      
+            "And burbled as it came! "
+      
+      "One, two! One, two! And through and through "
+            "The vorpal blade went snicker-snack! "
+      "He left it dead, and with its head "
+            "He went galumphing back. "
+      
+      "And hast thou slain the Jabberwock? "
+            "Come to my arms, my beamish boy! "
+      "O frabjous day! Callooh! Callay! "
+            "He chortled in his joy. "
+      
+      "Twas brillig, and the slithy toves "
+            "Did gyre and gimble in the wabe: "
+      "All mimsy were the borogoves, "
+            "And the mome raths outgrabe."  
+      
+            ;
+
+  // Text foreground color cycle effect
   uint8_t sector =0;
   uint8_t step=0;
     
@@ -868,8 +842,6 @@ void showjabber() {
 
   }
 
-
-
 }
 
 
@@ -884,7 +856,6 @@ void loop() {
   // TODO: Actually sample the state of the pullup on unused pins and OR it into the mask so we maintain the state.
   // Must do AFTER the cli(). 
   // TODO: Add offBits also to maintain the pullup state of unused pins. 
-
   
   return;  
 }
